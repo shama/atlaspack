43de7a2c7122cef3c3a22477b8251c7aee99547a
Ryan Mohr <ryan.mohr@gmail.com>
2015-10-27 15:06:48 -1000

    Allow canvas elements to be packed too
    Minor change that allows `<canvas>` elements to be packed too. Useful when you want to resize the images before packing them in the atlas.

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ Atlas.prototype._ontoCanvas = function(node) {
 // make sure we're always working with rects
 Atlas.prototype._toRect = function(rect) {
   // if rect is an image
-  if (rect.nodeName && rect.nodeName === 'IMG') {
+  if (rect.nodeName && (rect.nodeName === 'IMG' || rect.nodeName === 'CANVAS')) {
     this.img = rect;
     rect = new Rect(rect.x, rect.y, rect.width, rect.height);
   }
